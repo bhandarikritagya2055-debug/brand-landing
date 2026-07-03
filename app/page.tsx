0f15031ae6a9bb023c8be0247e7e07df50823e73
Kritagya Bhandari <bhandarikritagya2055@gmail.com>
2026-07-03 11:50:19 +0545

    Revert "add Flodesk popup form to landing page"
    This reverts commit 521a5c31ebd23b5401afbb12bec6f79a7166ca42.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Nav from "@/components/Nav";
 import ScrollDots from "@/components/ScrollDots";
-import FlodeskPopup from "@/components/FlodeskPopup";
 import HeroSection from "@/components/HeroSection";
 import AboutSection from "@/components/AboutSection";
 import ServicesSection from "@/components/ServicesSection";
@@ -13,7 +12,6 @@ import CTASection from "@/components/CTASection";
 export default function Home() {
   return (
     <>
-      <FlodeskPopup />
       <Nav />
       <ScrollDots />
       <main>
